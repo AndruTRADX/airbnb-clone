1a219ecd0bb264ddba9e6a2792e373fb50ffc376
Andrés Rodríguez <andrudeluxe@gmail.com>
2023-11-29 19:32:03 -0500

    base navbar done

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Container from '@/components/Container'
+import Logo from './Logo'
+import Search from './Search'
+import UserMenu from './UserMenu'
+
+const Navbar = () => {
+  return (
+    <nav className="fixed w-full shadow-sm bg-white/80 backdrop-blur z-10">
+      <div className="py-4 border-b">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        '3xl': '2520px'
+      },
+      maxWidth: {
+        'max-w-screen-3xl': '2520px'
+      }
     },
   },
   plugins: [],
